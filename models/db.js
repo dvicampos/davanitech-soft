7cfd75cb4d6df9bed7e539c8b32b6d35a72f60d1
dvicampos <dvicamp@gmail.com>
2025-04-26 22:21:16 -0600

    cambiio de vistas y graficas

--- models/db.js
+++ models/db.js
@@ -1,12 +1,20 @@
 const mysql = require('mysql2');
 
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'smboasmu_administrador',
+    password: 'ARno0192#',
+    database: 'smboasmu_davani',
+});
+/*
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'aseashvt_david',
     password: 'v_)hj(inH9b{',
     database: 'aseashvt_abogados_asociados',
 });
-
+*/
 db.connect(err => {
     if (err) {
         console.error('Error conectando a la base de datos: ' + err.stack);
